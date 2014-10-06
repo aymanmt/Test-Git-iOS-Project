1a9ed96be7c7802b244c98a689351b671c0e74cd
aymanmt <aymanmt@gmail.com>
2014-10-06 19:38:49 +0300

    added a test class

---
+++ Git Test/DBTest.h
@@ -0,0 +1,13 @@
+//
+//  DBTest.h
+//  Git Test
+//
+//  Created by admin on 10/6/14.
+//  Copyright (c) 2014 Deals Bender. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBTest : NSObject
+
+@end
---
+++ Git Test/DBTest.m
@@ -0,0 +1,13 @@
+//
+//  DBTest.m
+//  Git Test
+//
+//  Created by admin on 10/6/14.
+//  Copyright (c) 2014 Deals Bender. All rights reserved.
+//
+
+#import "DBTest.h"
+
+@implementation DBTest
+
+@end
